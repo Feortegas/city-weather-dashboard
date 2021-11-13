8fa09ecdf42ebb3ee9ab4d5c581c0d812b7660c6
Fernando Ortega <feortegas@gmail.com>
2021-11-13 01:25:27 -0600

    add data to 5 days forecast, add moment.js to handle date format

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,48 @@ const api_key = 'b24595aa58e805fb044c6ba2cad18792';
 
 var weather_icon = "http://openweathermap.org/img/w/";
 
-var theDate = new Date();
-
 var searchFormEl = document.querySelector("#form-sidebar");
 var cityNameEl = document.querySelector("#form-search-city");
+
+// current weather elements
 var cityDateEl = document.querySelector("#city-date");
 var cityWeatherIcon = document.querySelector("#weather-icon");
 var curTempEl = document.querySelector("#cur-temp");
 var curWindEl = document.querySelector("#cur-wind");
 var curHumidityEl = document.querySelector("#cur-humidity");
 var curUvIndexEl = document.querySelector("#cur-uv-index");
+
+// forecast elements fc1
+var cityFc1El = document.querySelector("#city-fc1");
+var iconFc1El = document.querySelector("#icon-fc1");
+var tempFc1El = document.querySelector("#temp-fc1");
+var windFc1El = document.querySelector("#wind-fc1");
+var humidityFc1El = document.querySelector("#humidity-fc1");
+// forecast elements fc2
+var cityFc2El = document.querySelector("#city-fc2");
+var iconFc2El = document.querySelector("#icon-fc2");
+var tempFc2El = document.querySelector("#temp-fc2");
+var windFc2El = document.querySelector("#wind-fc2");
+var humidityFc2El = document.querySelector("#humidity-fc2");
+// forecast elements fc3
+var cityFc3El = document.querySelector("#city-fc3");
+var iconFc3El = document.querySelector("#icon-fc3");
+var tempFc3El = document.querySelector("#temp-fc3");
+var windFc3El = document.querySelector("#wind-fc3");
+var humidityFc3El = document.querySelector("#humidity-fc3");
+// forecast elements fc4
+var cityFc4El = document.querySelector("#city-fc4");
+var iconFc4El = document.querySelector("#icon-fc4");
+var tempFc4El = document.querySelector("#temp-fc4");
+var windFc4El = document.querySelector("#wind-fc4");
+var humidityFc4El = document.querySelector("#humidity-fc4");
+// forecast elements fc5
+var cityFc5El = document.querySelector("#city-fc5");
+var iconFc5El = document.querySelector("#icon-fc5");
+var tempFc5El = document.querySelector("#temp-fc5");
+var windFc5El = document.querySelector("#wind-fc5");
+var humidityFc5El = document.querySelector("#humidity-fc5");
+
 
 // get weather data from API
 var getWeatherData = function (city) {
@@ -22,11 +54,10 @@ var getWeatherData = function (city) {
         .then(function(response) {
             // request was successful
             if (response.ok) {
-                console.log(response);
                 response.json().then(function (data) {
-                    console.log(data);
+                    // console.log(data);
                     getCurWeather(data.name, data.main.temp, data.wind.speed, data.main.humidity, data.clouds.all, data.weather[0].icon);
-                    getForecast();
+                    getOneCallApiData(data.coord.lat, data.coord.lon);
                   });
             } else {
                 alert("Error: OpenWeather - city not found");
@@ -45,9 +76,9 @@ var getOneCallApiData = function (lat, lon) {
         .then(function(response) {
             // request was successful
             if (response.ok) {
-                console.log(response);
                 response.json().then(function(data) {
                     console.log(data);
+                    getForecast(data);
                 });
             } else {
                 alert("Error: OpenWeather - Lat and Lon not found");
@@ -61,7 +92,7 @@ var getOneCallApiData = function (lat, lon) {
 // get current weather data
 var getCurWeather = function(city_name, cur_temp, cur_wind_speed, cur_humidity, cur_clouds, cur_icon) {
     if (getCurWeather) {
-        cityDateEl.textContent = city_name + " (" + theDate.getMonth() + "/" + theDate.getDay() + "/" + theDate.getFullYear() + ")";
+        cityDateEl.textContent = city_name + " (" + moment().format("L") + ")";
         cityWeatherIcon.src = weather_icon + cur_icon + ".png";
         curTempEl.textContent = "Temp: "+ cur_temp + "F";
         curWindEl.textContent = "Wind: "+ cur_wind_speed + "mph";
@@ -71,8 +102,51 @@ var getCurWeather = function(city_name, cur_temp, cur_wind_speed, cur_humidity, 
 };
 
 // get forecast for the next 5 days
-var getForecast = function () {
-
+var getForecast = function (data) {
+    if (getForecast) {
+        for (let index = 0; index < 5; index++) {
+            var fcData = data.daily[index];
+            if (index === 0) {
+                cityFc1El.textContent = moment().add(index + 1, 'days').format("L");
+                iconFc1El.src = weather_icon + fcData.weather[0].icon + ".png";
+                tempFc1El.textContent = "Temp: " + Math.round((Number(fcData.temp.min) + Number(fcData.temp.max))/2 *100)/100 + " F";
+                windFc1El.textContent = "Wind: " + fcData.wind_speed + " mph";
+                humidityFc1El.textContent = "Humidity: " + fcData.humidity + " %";
+            }
+            else if (index === 1) {
+                cityFc2El.textContent = moment().add(index + 1, 'days').format("L");
+                iconFc2El.src = weather_icon + fcData.weather[0].icon + ".png";
+                tempFc2El.textContent = "Temp: " + Math.round((Number(fcData.temp.min) + Number(fcData.temp.max))/2 *100)/100 + " F";
+                windFc2El.textContent = "Wind: " + fcData.wind_speed + " mph";
+                humidityFc2El.textContent = "Humidity: " + fcData.humidity + " %";
+            }
+            else if (index === 2) {
+                cityFc3El.textContent = moment().add(index + 1, 'days').format("L");
+                iconFc3El.src = weather_icon + fcData.weather[0].icon + ".png";
+                tempFc3El.textContent = "Temp: " + Math.round((Number(fcData.temp.min) + Number(fcData.temp.max))/2 *100)/100 + " F";
+                windFc3El.textContent = "Wind: " + fcData.wind_speed + " mph";
+                humidityFc3El.textContent = "Humidity: " + fcData.humidity + " %";
+            }
+            else if (index === 3) {
+                cityFc4El.textContent = moment().add(index + 1, 'days').format("L");
+                iconFc4El.src = weather_icon + fcData.weather[0].icon + ".png";
+                tempFc4El.textContent = "Temp: " + Math.round((Number(fcData.temp.min) + Number(fcData.temp.max))/2 *100)/100 + " F";
+                windFc4El.textContent = "Wind: " + fcData.wind_speed + " mph";
+                humidityFc4El.textContent = "Humidity: " + fcData.humidity + " %";
+            }
+            else if (index === 4) {
+                cityFc5El.textContent = moment().add(index + 1, 'days').format("L");
+                iconFc5El.src = weather_icon + fcData.weather[0].icon + ".png";
+                tempFc5El.textContent = "Temp: " + Math.round((Number(fcData.temp.min) + Number(fcData.temp.max))/2 *100)/100 + " F";
+                windFc5El.textContent = "Wind: " + fcData.wind_speed + " mph";
+                humidityFc5El.textContent = "Humidity: " + fcData.humidity + " %";
+            }
+            else {
+                return;
+            }
+            
+        }
+    }
 };
 
 // form search handler function
